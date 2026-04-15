b2be6762ed7158c2632470c8a1bee4010e858a70
Lucasbc47 <lucasbarboza1costa@gmail.com>
2026-04-15 00:16:41 -0300

    feat(p1): obtem condicoes atuais a partir de coords

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const axios = require("axios");
 
 const baseURL = `https://geocoding-api.open-meteo.com/v1/search`;
+const meteoURL = `https://api.open-meteo.com/v1/forecast`;
 
 function obterCord(cidade) {
   const param = `?name=${cidade}&count=1&language=pt&format=json`;
@@ -11,9 +12,25 @@ function obterCord(cidade) {
     };
   });
 }
+async function obterCond(lat, lon) {
+  const param = `?latitude=${lat}&longitude=${lon}&current=temperature_2m,apparent_temperature,wind_speed_10m`;
+  try {
+    const resp = await axios.get(meteoURL + param);
+    const atual = resp.data.current;
+    console.log(
+      `Temp: ${atual.temperature_2m}°C - sensação térmica: ${atual.apparent_temperature}°C`,
+    );
+    console.log(`Velocidade do Vento: ${atual.wind_speed_10m} km/h`);
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 let cidade = "Piracicaba";
 
 obterCord(cidade)
-  .then((coords) => console.log(`${cidade} (${coords.lat}, ${coords.lon})`))
+  .then((coords) => {
+    console.log(`${cidade} (${coords.lat}, ${coords.lon})`);
+    obterCond(coords.lat, coords.lon);
+  })
   .catch((err) => console.log(err));
