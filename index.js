e3425605b7fca887cd8bb36ebe2bc4be9e0ab5ef
Gabriel Rossi <Biel.invic@gmail.com>
2026-04-15 00:23:38 -0300

    feat(p1): obtem resumo da cidade

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const axios = require("axios");
 
 const baseURL = `https://geocoding-api.open-meteo.com/v1/search`;
 const meteoURL = `https://api.open-meteo.com/v1/forecast`;
+const baseWiki = `https://pt.wikipedia.org/api/rest_v1/page`;
 
 function obterCord(cidade) {
   const param = `?name=${cidade}&count=1&language=pt&format=json`;
@@ -25,6 +26,19 @@ async function obterCond(lat, lon) {
     console.log(err);
   }
 }
+function consultEnciclopedia(cidade){
+  const param = `/summary/${cidade}`;
+  return axios.get(baseWiki + param, 
+  {
+    headers: {'User-Agent': 'paoo-p1/1.0'}
+  },
+).then((resp) =>{
+    return{
+      title: resp.data.title,
+      extract: resp.data.extract,
+    };
+  });
+}
 
 let cidade = "Piracicaba";
 
@@ -33,4 +47,8 @@ obterCord(cidade)
     console.log(`${cidade} (${coords.lat}, ${coords.lon})`);
     obterCond(coords.lat, coords.lon);
   })
+  .catch((err) => console.log(err));
+
+consultEnciclopedia(cidade)
+  .then((info) => console.log (`${info.title}, ${info.extract}`))
   .catch((err) => console.log(err));
